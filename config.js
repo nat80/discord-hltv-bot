0f4f170658e516f08d15946d2717c761c7734484
nat80 <37385071+nat80@users.noreply.github.com>
2019-03-05 03:21:22 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+// Your prefered prefix for the commands
+export const prefix = '!';
+
+// Your bot user token
+export const token = 'NTUyMzE0Mjk1NTU2NTA1NjI4.D19ugA.zkUeEuoJAgrbeVSqRRsbINK6Drk';
+
+// Your prefered teams to get next matches
+export const myTeams = [
+  'Cloud9',
+  'FaZe',
+  'SK',
+  'Natus Vincere',
+  'mousesports',
+  'Quantum Bellator Fire',
+  'G2',
+  'Fnatic',
+  'Gambit',
+  'Vega Squadron',
+  'Space Soldiers',
+  'BIG',
+  'Astralis',
+  'Liquid',
+  'North',
+  'Virtus.pro',
+  'Furious Gaming',
+  'Isurus',
+  'Coscu Army',
+  'OpTic'
+];
