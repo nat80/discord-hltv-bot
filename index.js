b4a2310f9e035cea386aa39d9f4c25b6f73d38ee
nat80 <37385071+nat80@users.noreply.github.com>
2019-03-05 05:13:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import Discord from 'discord.js';
 import { nextMatchDetails, nextMatchAnnouncer } from './modules/hltv';
 import { prefix, token } from './config';
 
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
 
 const client = new Discord.Client();
+
+var port = process.env.PORT || 3000;
+client.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 
 client.on('message', msg => {
   const { content } = msg;
